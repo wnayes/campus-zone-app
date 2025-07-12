20e4c4f33b77ec89e84601a701079d556daf600b
Will Nayes <wnayes@gmail.com>
2025-07-12 17:47:15 -0500

    Remove unchecked build flag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,9 +30,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-        tasks.withType<JavaCompile>().configureEach {
-            options.compilerArgs.add("-Xlint:unchecked")
-        }
     }
     kotlinOptions {
         jvmTarget = "11"
